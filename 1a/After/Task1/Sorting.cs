bd2f117e2e53e061795cfdb7b8fc8a382529cf2e
TEGTO <90476119+TEGTO@users.noreply.github.com>
2023-06-16 13:51:33 +0300

    pdf update

--- 1a/After/Task1/Sorting.cs
+++ 1a/After/Task1/Sorting.cs
@@ -1,45 +1,44 @@
-﻿using OOP_Lab_1;
-using System;
+﻿using System;
 using Task1.MyList;
 
 namespace Task1.Sorting
 {
 
 
-public class SortingEventArgs<T> : EventArgs where T : IComparable
-{
-    public MyList<T> myList;
-   
+    public class SortingEventArgs<T> : EventArgs where T : IComparable
+    {
+        public MyList<T> myList;
 
-}
 
-/// <summary>
-/// The sort class that will contain different sort subscribers.
-/// </summary>
+    }
 
-public class SortEventHandler<T> where T : IComparable
-{
     /// <summary>
-    /// Event for subscribing different sort methods.
-    /// </summary>
-    public event EventHandler<SortingEventArgs<T>> sortMethod;
-    /// <summary>
-    /// The method for sorting **MyList list.**
+    /// The sort class that will contain different sort subscribers.
     /// </summary>
 
-    public void Sort(MyList <T> list)
+    public class SortEventHandler<T> where T : IComparable
+    {
+        /// <summary>
+        /// Event for subscribing different sort methods.
+        /// </summary>
+        public event EventHandler<SortingEventArgs<T>> sortMethod;
+        /// <summary>
+        /// The method for sorting **MyList list.**
+        /// </summary>
+
+        public void Sort(MyList<T> list)
         {
             StartSorting(list);
-      
+
         }
-    /// <summary>
-    /// Protected method where the **MyList list** connects to the class.
-    /// </summary>
-    /// <param name="list">List for sorting.</param>
-    protected virtual void StartSorting(MyList<T> list)
+        /// <summary>
+        /// Protected method where the **MyList list** connects to the class.
+        /// </summary>
+        /// <param name="list">List for sorting.</param>
+        protected virtual void StartSorting(MyList<T> list)
         {
-        sortMethod?.Invoke(this, new SortingEventArgs <T>{ myList = list });
+            sortMethod?.Invoke(this, new SortingEventArgs<T> { myList = list });
         }
-}
+    }
 
 }
